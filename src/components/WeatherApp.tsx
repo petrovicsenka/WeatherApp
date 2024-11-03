d73c51ac1e114d633b9d76e33969b95a381576f7
Senka Petrovic <petrovicsenka@outlook.com>
2024-11-03 21:45:23 +0100

    Fetched data from OpenWeatherMap API; displayed location

--- src/components/WeatherApp.tsx
+++ src/components/WeatherApp.tsx
@@ -1,21 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import sunny from '../assets/images/sunny.png';
 import cloudy from '../assets/images/cloudy.png';
 import rainy from '../assets/images/rainy.png';
 import snowy from '../assets/images/snowy.png';
 
+// me - because I use TS:
+interface WeatherData {
+  // me: to check what exactly is needed
+  name: string;
+  main: {
+    temp: number;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
+  };
+}
+
 const WeatherApp = () => {
+  // const [data, setData] = useState({});
+  // me - because I use TS:
+  const [data, setData] = useState<WeatherData>({
+    name: '',
+    main: {
+      temp: 0,
+      humidity: 0,
+    },
+    wind: {
+      speed: 0,
+    },
+  });
+
+  const [location, setLocation] = useState('');
+  const api_key = '339898cf98c9635ca9c89a112b73f12d';
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLocation(e.target.value);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') search();
+  };
+
+  const search = async () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=${api_key}`;
+    const res = await fetch(url);
+    const searchData = await res.json();
+    console.log(searchData);
+    setData(searchData);
+    setLocation('');
+  };
+
   return (
     <div className='container'>
       <div className='weather-app'>
         <div className='search'>
           <div className='search-top'>
             <i className='fa-solid fa-location-dot'></i>
-            <div className='location'>London</div>
+            <div className='location'>{data.name}</div>
           </div>
           <div className='search-bar'>
-            <input type='text' placeholder='Enter Location' />
-            <i className='fa-solid fa-magnifying-glass'></i>
+            <input
+              type='text'
+              placeholder='Enter Location'
+              value={location}
+              onChange={handleInputChange}
+              onKeyDown={handleKeyDown}
+            />
+            <i className='fa-solid fa-magnifying-glass' onClick={search}></i>
           </div>
         </div>
         <div className='weather'>
